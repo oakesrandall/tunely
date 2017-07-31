a4189fb4862f6d727345d9b122c9c303d845a55f
David_Randall <oakesrandall@gmail.com>
2017-07-31 14:56:47 -0600

    step 3 done

--- models/album.js
+++ models/album.js
@@ -1,2 +1,13 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+let albumSchema = new Schema ({
+        artistName: String,
+        name: String,
+        releaseDate: String,
+        genres: [ String ]
+});
+
+let Album = mongoose.model('Album', albumSchema);
+
+module.exports = Album;
